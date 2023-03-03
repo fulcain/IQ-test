ed4b7c0482f6eaa80ba3ccb5777515a5b27b962c
fulcain <majidkargar01@gmail.com>
2023-03-03 15:56:34 +0330

    deleted extra code in questionBox function

--- JS/script.js
+++ JS/script.js
@@ -149,13 +149,6 @@ function questionBox() {
     minutesEl = document.querySelector('#minutes')
     secondsEl = document.querySelector('#seconds')
     wholeQuestionBox = document.querySelectorAll('.whole-question-box')
-
-    // changes the style if answers are 2 rows
-    if (currentQuestion > 12) {
-        wholeQuestionBox.forEach(item => {
-            item.style.minHeight = "600px"
-        })
-    }
 }
 
 // TITLE: answers function
